2d283a79812b283b9fcd4ac533a6acfd770f00f1
ShabdRastogi <shabdrastogi76@gmail.com>
2026-09-16 11:30:19 +0530

    feat: add url routing and initial templates

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-  path("",views.main,name='main')
+  path("",views.main,name='main'),
+  path("register/",views.register,name='register'),
+  path("login/",views.login,name='login'),
 ]
--- main/views.py
+++ main/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 
 def main(request):
-  return HttpResponse("hello world")  
+  return render(request,'main.html') 
+
+def register(request):
+  return render(request,'register.html') 
+
+def login(request):
+  return render(request,'login.html')
