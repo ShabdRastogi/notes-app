c0fd4b3bac920aea703a062bd48375e0fd2046cb
ShabdRastogi <shabdrastogi76@gmail.com>
2026-09-16 15:38:40 +0530

    style: add styling to templates

--- main/views.py
+++ main/views.py
@@ -4,10 +4,10 @@ from django.contrib.auth import login,authenticate,logout
 from django.contrib.auth.decorators import login_required
 from .models import Notes
 
+@login_required
 def main(request):
-    if not request.user.is_authenticated:
-        return redirect('login')
-    return render(request,'main.html')
+    notes = Notes.objects.filter(user=request.user)
+    return render(request,'main.html',{'notes':notes})
 def register_view(request):
   if request.method == 'POST':
     username = request.POST['username']
