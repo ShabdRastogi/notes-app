43060ce1d93a59973d2d8ee3837ef212294173c4
ShabdRastogi <shabdrastogi76@gmail.com>
2026-09-18 02:02:50 +0530

    feat: connect PostgreSQL database

--- notesapp/settings.py
+++ notesapp/settings.py
@@ -85,6 +85,8 @@ DATABASES = {
     }
 }
 
+DATABASES['default'] =dj_database_url.parse(os.getenv("DATABASE_URL"))
+
 AWS_ACCESS_KEY_ID=os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY=os.environ.get("AWS_SECRET_ACCESS_KEY")
 
