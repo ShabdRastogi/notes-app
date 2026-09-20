4554feb6b1d8fdcf5a91d56c5def9202e735ab91
ShabdRastogi <shabdrastogi76@gmail.com>
2026-09-20 15:22:34 +0530

    chore: clean up unused files and folders

--- notesapp/settings.py
+++ notesapp/settings.py
@@ -104,13 +104,10 @@ WSGI_APPLICATION = 'notesapp.wsgi.application'
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        os.getenv("DATABASE_URL")
+    )
 }
-
-DATABASES['default'] =dj_database_url.parse(os.getenv("DATABASE_URL"))
 
 AWS_ACCESS_KEY_ID=os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY=os.environ.get("AWS_SECRET_ACCESS_KEY")
